1e2aebf19d8aba854c474dfba18e1c1ba547c125
systemsevenfive <ryanwsaunders@icloud.com>
2023-02-27 19:39:28 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ryan Saunders on 2023-02-27.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
